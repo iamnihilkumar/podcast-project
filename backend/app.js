96cb1263a6d9983e9d182ce4b5b899796afaeda6
Nikhil <nikhilsharma75612@gmail.com>
2026-05-04 23:13:33 +0530

    added Routes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const userApi = require("./routes/user");
 require("dotenv").config();
 require("./conn/conn");
+app.use(express.json());
+
+//all routes
+app.use("/api/v1", userApi);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port : ${process.env.PORT}`);
-})
+});
