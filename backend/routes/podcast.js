573629eef5e25ee7ac54f7c35572d76ecd4a54b2
Nikhil <nikhilsharma75612@gmail.com>
2026-06-13 23:56:12 +0530

    Setting Podcast apis

--- backend/routes/podcast.js
+++ backend/routes/podcast.js
@@ -1,13 +1,15 @@
 const authMiddleware = require("../middleware/authMiddleware");
 const upload = require("../middleware/multer");
-const Category = require("../models/Category");
+const Category = require("../models/category");
 const Podcast = require("../models/podcast");
+const User = require("../models/user");
 
 const router = require("express").Router();
 
 //add-podcast
 router.post("/add-podcast", authMiddleware, upload, async (req, res) => {
-  const { title, description, category } = req.body;
+  try {
+    const { title, description, category } = req.body;
   const frontImage = req.files["frontImage"][0].path;
   const audioFile = req.files["audioFile"][0].path;
   if(!title || !description || !category || !frontImage || !audioFile){
@@ -28,5 +30,52 @@ router.post("/add-podcast", authMiddleware, upload, async (req, res) => {
     audioFile, 
     user : userid
   });
+  await newPodcast.save();
+  await Category.findByIdAndUpdate(catid, {
+    $push : { podcasts : newPodcast._id } 
+  });
+  await User.findByIdAndUpdate(userid, {
+    $push : { podcasts : newPodcast._id } 
+  });
+  res.status(201).json({message : "Podcast added successfully"});
+  } catch (error) {
+
+    res.status(500).json({message : "Failed to add podcast"});
+  }
+
+});
+
+//get all podcasts
+router.get("/get-podcasts", async (req, res) => {
+  try {
+    const podcasts = await Podcast.find()
+    .populate("category")
+    .sort({ createdAt : -1 });
+    return res.status(200).json({data : podcasts});
+  } catch (error) {
+    return res.status(500).json({message : "Internal Server Error"});
+  }
+});
+
+//get-user-podcasts
+router.get("/get-user-podcasts",authMiddleware, async (req, res) => {
+  try {
+    const { user } = req;
+    const userid = user._id;
+    const data = await User.findById(userid)
+    .populate({
+      path :"podcasts", 
+      populate : { path : "category" },
+    })
+    .select("-password");
+    if(data && data.podcasts){
+      data.podcasts.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+    }
+    return res.status(200).json({data : podcasts});
+  } catch (error) {
+    return res.status(500).json({message : "Internal Server Error"});
+  }
 });
 module.exports = router;
